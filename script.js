9f55cf3659f1be3cdc282c22a3af02de03d933d2
Ashu <63907978+ash-negee@users.noreply.github.com>
2024-05-31 16:17:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    // Get the text field
+    var copyText = 5142794354
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText);
+  }
