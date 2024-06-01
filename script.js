0f24c45848d7c930d3e083aaaafd135e669e3dfd
Ashu <63907978+ash-negee@users.noreply.github.com>
2024-06-01 13:30:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-function myFunction() {
-    // Get the text field
-    var copyText = 5142794354
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText);
-  }
+/**
+ * 
+ * @param {*} text parameter recieved from calling
+ */
+function copyText(text){
+  let elementText = text;
+  let inputElement = document.createElement('input');
+  inputElement.setAttribute('value', elementText);
+  document.body.appendChild(inputElement);
+  inputElement.select();
+  document.execCommand('copy');
+  inputElement.parentNode.removeChild(inputElement);
+}
+
+// for preloader
+function preloader(){
+  var loader = document.getElementById("pre-loader")
+  window.addEventListener("load", function(){
+    loader.style.display = "none";
+  })
+}preloader();
+
+//tooltip
+function showCopied() {
+  const copiedPrompt = document.getElementById("copied-prompt");
+  copiedPrompt.style.visibility = "visible";
+
+  // Hide the prompt after a certain duration (e.g., 2 seconds)
+  setTimeout(function() {
+    copiedPrompt.style.visibility = "hidden";
+  }, 2000);
+}
+
