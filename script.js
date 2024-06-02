cf7c8b27e2abd4405c8a5f2c8f18edf1762ec528
Ashu <63907978+ash-negee@users.noreply.github.com>
2024-06-02 11:34:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
  * 
  * @param {*} text parameter recieved from calling
  */
-function copyText(text){
-  let elementText = text;
+function copyText(gameId){
+  let elementText = gameId;
   let inputElement = document.createElement('input');
   inputElement.setAttribute('value', elementText);
   document.body.appendChild(inputElement);
@@ -14,7 +14,7 @@ function copyText(text){
 
 // for preloader
 function preloader(){
-  var loader = document.getElementById("pre-loader")
+  var loader = document.getElementById("pre-loader");
   window.addEventListener("load", function(){
     loader.style.display = "none";
   })
@@ -25,7 +25,7 @@ function showCopied() {
   const copiedPrompt = document.getElementById("copied-prompt");
   copiedPrompt.style.visibility = "visible";
 
-  // Hide the prompt after a certain duration (e.g., 2 seconds)
+// Hide the prompt after a certain duration (e.g., 2 seconds)
   setTimeout(function() {
     copiedPrompt.style.visibility = "hidden";
   }, 2000);
